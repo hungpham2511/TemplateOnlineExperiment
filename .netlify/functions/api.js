558d5d3810d4292773dd2984b6a3ad10fa8f4b51
Hung <hungpham2511@gmail.com>
2021-03-09 23:22:47 +0800

    Update API

--- .netlify/functions/api.js
+++ .netlify/functions/api.js
@@ -1,13 +1,36 @@
 //upload data
 //authenticate with octokit
-
 const { Octokit } = require("@octokit/rest");
 
+// Commit info
+const REPO_NAME = "TemplateOnlineExperiment"
+const REPO_OWNER = "hungpham2511" // update this to use "RealityBending"
+
 exports.handler = async (event, context) => {
+    // content should be a dictionary with two attributes: 'path' and 'data'
+    const content = JSON.parse(event.body);
+    console.log(content)
+    
     const octokit = new Octokit({
         auth: process.env.GH_OAUTH, // authenticating Octokit
     })
-    const { data } = await octokit.request("/user");
+    const response = await octokit.repos.createOrUpdateFileContents({
+        owner: REPO_OWNER,
+        repo: REPO_NAME,
+        content: Buffer.from(content.data).toString('base64'),
+        message: `Saving ${content.path}`,
+        path: content.path, 
+        committer: {
+            name: "Hung",
+            email: "hungpham2511@gmail.com",
+        },
+        author: {
+            name: "Hung",
+            email: "hungpham2511@gmail.com",
+        }
+    })
+    console.log(response)
+
     return {
         statusCode: 200,
         body: JSON.stringify({
